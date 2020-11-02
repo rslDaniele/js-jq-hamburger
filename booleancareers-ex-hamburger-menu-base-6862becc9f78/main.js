92acfa81bf12833dc8548230d98fc41372bd6567
Daniele <danlup94@gmail.com>
2020-11-02 15:49:40 +0100

    Esercizio completato

---
+++ booleancareers-ex-hamburger-menu-base-6862becc9f78/main.js
@@ -0,0 +1,9 @@
+// Rendere il menu hamburger visibile al click
+
+$("a").click(function functionName() {
+  $(".hamburger-menu").show('slow');
+});
+
+$(".close").click(function functionName() {
+  $(".hamburger-menu").hide('slow');
+});
